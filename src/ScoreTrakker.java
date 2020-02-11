76cb86a07916083a1683a3d3887a3d288dd0697b
Daniel Simmons <desimmons@mines.edu>
2020-02-11 13:45:02 -0700

    loadDataFromFile complete, started last part

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -11,15 +11,22 @@ public class ScoreTrakker {
 
 	private ArrayList<Student> students = new ArrayList<Student>();
 	
+	// Stores strings to use as filenames into loadDataFromFile
+	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" };
+	
 	// Reads student data from file with inputed name and stores student objects in the ArrayList
 	private void loadDataFromFile(String fileName) throws FileNotFoundException {
 		FileReader reader = new FileReader(fileName);
 		Scanner in = new Scanner(reader);
 		while (in.hasNextLine()) {
 			String name = in.nextLine();
-			int score = in.nextInt();
-			if (in.hasNextLine()) in.nextLine(); // Skips over rest of the line after the int
-			students.add(new Student(name, score));
+			String scoreString = in.nextLine(); // Takes in entire next line, which should be the score value
+			try {
+				int score = Integer.parseInt(scoreString);
+				students.add(new Student(name, score));
+			} catch (Exception NumberFormatException) {
+				System.out.println("Incorrect format for " + name + "not a valid score: " + scoreString);
+			}
 		}
 		in.close();
 	}
@@ -34,7 +41,9 @@ public class ScoreTrakker {
 	
 	// 
 	private void processFiles() throws FileNotFoundException {
-		loadDataFromFile("scores.txt");
+		for (String fileName : files) {
+			loadDataFromFile(fileName);
+		}
 		printInOrder();
 	}
 	
